ad6c4174ffc32e31c2bf75315f1c08232b21f43c
David (D.J) <david.joset@yahoo.fr>
2020-11-26 10:49:43 -0500

    Add project files.

---
+++ DownloadFileFromFileSystemWebApplication/Controllers/DocumentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DownloadFileFromFileSystemWebApplication.Helper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DownloadFileFromFileSystemWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+        [HttpGet("{fileName}/data")]
+        public FileStreamResult FinDocumentData(string fileName)
+        {
+            var filePath = FileHelper.GetFilePathFromListWithFileNameParameter(fileName);
+            //var filePath = FileHelper.GetFilePath(fileName);
+            var fileContent = System.IO.File.ReadAllBytes(filePath);
+            var ms = new MemoryStream(fileContent);
+            var fileType = "text/plain";
+            var result = new FileStreamResult(ms, fileType);
+
+            result.FileDownloadName = fileName + Path.GetExtension(filePath);
+
+            return result;
+        }
+    }
+}
---
+++ DownloadFileFromFileSystemWebApplication/Helper/FileHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DownloadFileFromFileSystemWebApplication.Helper
+{
+    public class FileHelper
+    {
+        public static List<string> GetListOfFilePath(string filePath = @"D:\temp")
+        {
+            return Directory.GetFiles($@"{filePath}\").ToList();
+        }
+
+        public static string GetFilePath(string fileName)
+        {
+            return @"D:\temp" + $@"\{fileName}" + ".txt";
+        }
+
+        //public static string GetFilePathFromListWithFileNameParameter(List<string> listOfFilePath, string fileName)
+        //{
+        //    return listOfFilePath.FirstOrDefault(filePath => Path.GetFileNameWithoutExtension(filePath) == fileName);
+        //}
+        public static string GetFilePathFromListWithFileNameParameter(string fileName)
+        {
+            return GetListOfFilePath().FirstOrDefault(filePath => Path.GetFileNameWithoutExtension(filePath) == fileName);
+        }
+    }
+}
