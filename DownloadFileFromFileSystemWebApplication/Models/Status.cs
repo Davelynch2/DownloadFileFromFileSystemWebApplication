4f6e6463ba65f04bbbee74b887b98f18bd83a7c7
David (D.J) <david.joset@yahoo.fr>
2020-11-30 12:32:14 -0500

    add status enum

---
+++ DownloadFileFromFileSystemWebApplication/Models/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DownloadFileFromFileSystemWebApplication.Models
+{
+    public enum Status
+    {
+        Create = 0,
+        Modify = 1,
+        Delete = 2
+    }
+}
