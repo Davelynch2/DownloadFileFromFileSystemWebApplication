717c2f724987000da23f152711036a1b5c0bec49
David (D.J) <david.joset@yahoo.fr>
2020-11-30 12:32:01 -0500

    Add models : doc and doc upload

---
+++ DownloadFileFromFileSystemWebApplication/Models/Document.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DownloadFileFromFileSystemWebApplication.Models
+{
+    public class Document
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [JsonPropertyName("id")]
+        public Guid Id { get; }
+
+        [Required]
+        [JsonPropertyName("revision")]
+        public int Revision { get; private set; }
+
+        [Required(ErrorMessage = "The name attribute is missing.")]
+        [StringLength(100, MinimumLength = 1)]
+        [JsonPropertyName("name")]
+        public string Name { get; private set; } = string.Empty;
+
+        [StringLength(250, MinimumLength = 0)]
+        [JsonPropertyName("location")]
+        public string Location { get; private set; } = string.Empty;
+
+        [Required(ErrorMessage = "The type attribute is missing.")]
+        [StringLength(50, MinimumLength = 1)]
+        [JsonPropertyName("fileType")]
+        public string FileType { get; private set; } = string.Empty;
+
+        [Required(ErrorMessage = "The size attribute is missing.")]
+        [JsonPropertyName("size")]
+        public long Size { get; private set; } = 0;
+
+        //[Required(ErrorMessage = "The status attribute is missing.")]
+        //[JsonPropertyName("status")]
+        //public Status Status { get; set; }
+
+        [Required(ErrorMessage = "The lastTimeModified attribute is missing.")]
+        [JsonPropertyName("lastTimeModified")]
+        public DateTime LastTimeModified { get; private set; } = DateTime.UtcNow;
+
+        private string _location => @"D:\DocumentStorage";
+
+        public Document() {}
+
+        public Document(DocumentUpload documentUpload)
+        {
+            Id = Guid.NewGuid();
+            Revision = 0;
+
+            if (!string.IsNullOrEmpty(documentUpload.Name))
+                Name = documentUpload.File.FileName.Split('.').First();
+
+            if (documentUpload.File != null)
+            {
+                FileType = documentUpload.File.ContentType;
+                Location = $@"{_location}\{documentUpload.File.FileName}";
+                Size = documentUpload.File.Length;
+            }
+
+            //Status = Status.Create;
+        }
+
+    }
+}
---
+++ DownloadFileFromFileSystemWebApplication/Models/DocumentUpload.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DownloadFileFromFileSystemWebApplication.Models
+{
+    public class DocumentUpload
+    {
+        [Required(ErrorMessage = "The name attribute is missing.")]
+        [StringLength(100, MinimumLength = 1)]
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("file")]
+        public IFormFile File { get; set; }
+
+        public byte[] GetByteArrayData()
+        {
+            using var target = new MemoryStream();
+            File.OpenReadStream().CopyTo(target);
+            return target.ToArray();
+        }
+
+    }
+}
